1a27186fc4be180f0c3f1029d0487ab7a021aa8d
Bonsa Beker <bakerboonsa@gmail.com>
2025-07-15 23:12:37 +0300

    good front

--- travel-app/src/app/page.tsx
+++ travel-app/src/app/page.tsx
@@ -5,6 +5,24 @@ export default function Home() {
    <div>
     <Navbar/>
     <h1 className="text-6xl font-bold text-center mt-10 ">Where to?</h1>
+    <p className="text-center text-lg  mt-4">Explore the beauty of Oromia</p>
+
+    <div className="flex justify-center mt-20">
+  <div className="flex w-full max-w-xl">
+    <input
+      type="text"
+      placeholder="Search tours..."
+      className="flex-grow rounded-l-md border border-gray-300 px-4 py-2 focus:outline-none focus:ring-2 focus:ring-green-500"
+    />
+    <button
+      className="bg-green-600 text-white px-4 py-2 rounded-r-md hover:bg-green-700 transition-colors"
+    >
+      Search
+    </button>
+  </div>
+</div>
+
+
 
    </div>
         
